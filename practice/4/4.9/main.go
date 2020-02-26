b7ee7514d52013c5df67928a67ff4acb63d44953
ChangXin <1561753947@qq.com>
2020-02-26 22:42:11 +0800

    finish 4.9

--- practice/4/4.9/main.go
+++ practice/4/4.9/main.go
@@ -7,50 +7,23 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"os"
-	"unicode"
-	"unicode/utf8"
 )
 
 func main() {
-	counts := make(map[string]int) // Unicode 字符数量
-	counts["letter"] = 0
-	counts["num"] = 0
-	counts["others"] = 0
-	counts["invalid"] = 0
-	var utflen [utf8.UTFMax + 1]int // UTF-8 编码的长度
-	in := bufio.NewReader(os.Stdin)
-	for {
-		r, n, err := in.ReadRune()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "charcount:%v\n", err)
-			os.Exit(1)
-		}
-		if r == unicode.ReplacementChar && n == 1 {
-			counts["invalid"]++
-			continue
-		}
-		if unicode.IsLetter(r) {
-			counts["letter"]++
-		}
-		if unicode.IsNumber(r) {
-			counts["num"]++
-		}
-		counts["others"]++
-		utflen[n]++
+	seen := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	input.Split(bufio.ScanWords)
+	for input.Scan() {
+		seen[input.Text()]++
 	}
-	fmt.Printf("type\t\tcount\n")
-	for c, n := range counts {
-		fmt.Printf("%s\t\t%d\n", c, n)
+	if err := input.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "wordfreq :%v\n", err)
+		os.Exit(1)
 	}
-	fmt.Printf("\nlrn\tcount\n")
-	for i, n := range utflen {
-		if i > 0 {
-			fmt.Printf("%d\t%d\n", i, n)
-		}
+	fmt.Printf("word\tnum\n")
+	for k, v := range seen {
+		fmt.Printf("%s\t%d\n", k, v)
 	}
+
 }
