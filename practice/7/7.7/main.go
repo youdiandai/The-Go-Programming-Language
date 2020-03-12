118ba338484abdf4a866ced6065422ada9b12562
ChangXin <1561753947@qq.com>
2020-03-12 23:36:08 +0800

    finish 7.6 7.7

---
+++ practice/7/7.7/main.go
@@ -0,0 +1,10 @@
+/*
+因为默认值20.0的类型为Celsius类型,而该类型又定义了String方法，也就是定义了默认的输出格式，会自动在数字后面加上°C
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("因为默认值20.0的类型为Celsius类型,而该类型又定义了String方法，也就是定义了默认的输出格式，会自动在数字后面加上°C")
+}
